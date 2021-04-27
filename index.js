2a29812dfee3802640630dcbb9099e0317b51bd6
mounikareddykunduru <mounikareddykunduru@tamu.edu>
2021-04-27 14:11:08 -0500

    index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const args = process.argv.slice(2);
-console.log(args);
-console.log(args.length);
+//console.log(args);
+//console.log(args.length);
 function splitToDigit(n){
     return [...n + ''].map(Number)
   }
@@ -10,8 +10,8 @@ let phonetic = new Map([
     [2, 'Two'], [3, 'Three'],[4, 'Four'],[5, 'Five'],
     [6, 'Six'],[7, 'Seven'],[8, 'Eight'],[9, 'Nine']
 ]);
-console.log(args[0]);
-console.log(args[1]);
+//console.log(args[0]);
+//console.log(args[1]);
 var newarr=[];
 var z= new Array(args.length);
 console.log(z);
